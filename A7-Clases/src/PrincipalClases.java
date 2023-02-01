4a280e445edd950c3f77a8b6233abdb4477f9783
AlejandroCamargoA <20211D115@khipu.edu.pe>
2023-01-31 21:29:10 -0500

    Menus Incompleto

--- A7-Clases/src/PrincipalClases.java
+++ A7-Clases/src/PrincipalClases.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class PrincipalClases {
@@ -13,17 +14,8 @@ public class PrincipalClases {
         }
     }
 
-    public static void mostrarFlorPos(int posicion, List<Cflor> flores){
-        int pos = 0;
-        Boolean flag = true;
-        do{
-            if(posicion == pos) {
-                //flores.get(posicion).mostrar();
-                flores.get(posicion).mostrar();
-                flag = false;
-            }
-            pos++;
-        }while (flag);
+    public static void mostrarFlorPos(String att1, String att2, List<Objects> objetos){
+
     }
 
     public static void menuPrincipal(){
@@ -33,117 +25,96 @@ public class PrincipalClases {
         System.out.println("2- Gestionar Clientes.");
         System.out.println("3- Gestionar Vendedores.");
         System.out.println("4- Gestionar Ventas.");
-        System.out.println("  << Ingrese una opcion >>  ");
+        System.out.println("5- Gestionar DetalleVentas.");
+        System.out.println("  << Ingrese una opción >>  ");
     }
-    public static void menuFlores(){
-        System.out.println("*********** MENU FLORES ***********");
+    public static void menuClases(String ClaseS){
+        System.out.println("*********** MENU " + ClaseS + " ***********");
         System.out.println("0- Salir.");
         System.out.println("1- (C) Crear (Create).");
         System.out.println("2- (R) Mostrar (Read).");
         System.out.println("3- (U) Actualizar (update).");
         System.out.println("4- (D) Eliminar (Delete).");
-        System.out.println("   Ingrese una opcion>>  ");
+        System.out.println("  <<Ingrese una opción >>  ");
     }
-
+    public static void subMenus(String ClaseS, String opcion){
+        System.out.println("  Menu " + opcion + " " + ClaseS );
+        System.out.println(" 0- Regresar al menu " + ClaseS);
+        System.out.println(" 1- " + opcion + " por clave");
+        System.out.println(" 2- " + opcion + " todo " + ClaseS);
+        System.out.println("  << Ingrese una opción >>  ");
+    }
     public static void main(String[] args) {
-        List<Cflor> flores = new ArrayList<Cflor>();
-        List<CCliente> clientes = new ArrayList<CCliente>();
-        List<CVendedor> vendedores = new ArrayList<CVendedor>();
-        List<CVenta> ventas = new ArrayList<CVenta>();
-        List<CDetalleVenta> detalleVenta = new ArrayList<CDetalleVenta>();
         int opcionPrincipal;
         do{
             menuPrincipal();
-            opcionPrincipal = new Scanner(System.in).nextInt();
+            opcionPrincipal = FuncionesBasicas.leerEntero();
             switch(opcionPrincipal){
                 case 0:
                     System.out.println("Adios ...");
                     break;
                 case 1:
-                    int opcionFlores;
+                    int opcionflor;
                     do {
-                        menuFlores();
-                        opcionFlores = new Scanner(System.in).nextInt();
-                        switch (opcionFlores) {
+                        menuClases("FLORES");
+                        opcionflor = FuncionesBasicas.leerEntero();
+                        switch (opcionflor){
                             case 0:
-                                System.out.println("Regresando al menu Principal...");
+                                System.out.println("Regresando al menú principal...");
                                 break;
-                            case 1:  // Insertar // Crear
-                                Cflor flor = new Cflor();
-                                flores.add(flor);
+                            case 1:
+                                System.out.println("Ingrese la nueva flor");
+                                //
                                 break;
-                            case 2: // Mostrar
-                                System.out.println("  Menu Mostrar Flor ");
-                                System.out.println(" 0- Regresar al menu Flores");
-                                System.out.println(" 1- Mostrar por posicion");
-                                System.out.println(" 2- Mostrar todas las flores");
-                                System.out.println("   Ingrese una opcion>>  ");
-                                int opcionMostrarFlores = new Scanner(System.in).nextInt();
-                                switch (opcionMostrarFlores) {
-                                    case 0:
-                                        System.out.println(" ... Regresando al Menu Flores");
-                                        break;
-                                    case 1:
-                                        System.out.println("Ingrese la posicion de la flor buscada: ");
-                                        int posicion = new Scanner(System.in).nextInt();
-                                        mostrarFlorPos(posicion, flores);
-                                        break;
-                                    case 2:
-                                        mostrarFlores(flores);
-                                        break;
-                                }
+                            case 2:
+                                subMenus("Flores", "Mostrar");
+                                int opcionMostrarFlores;
+                                do {
+                                    opcionMostrarFlores = FuncionesBasicas.leerEntero();
+                                    switch (opcionMostrarFlores){
+                                        case 0:
+                                            System.out.println("Regresando al menú flores");
+                                            break;
+                                        case 1:
+                                            //
+                                            break;
+                                        case 2:
+                                            //
+                                            break;
+
+                                    }
+                                }while (opcionMostrarFlores != 0);
+
                                 break;
-                            case 3:  // Actualizar
-                                System.out.println("  Menu Mostrar Flor ");
-                                System.out.println(" 0- Regresar al menu Flores");
-                                System.out.println(" 1- Actualizar un campo");
-                                System.out.println(" 2- Actualizar todo");
-                                System.out.println("   Ingrese una opcion>>  ");
-                                int opcionActualizarFlor = new Scanner(System.in).nextInt();
-                                switch (opcionActualizarFlor) {
-                                    case 0:
-                                        System.out.println(" ... Regresando al menu Flores");
-                                        break;
-                                    case 1:
-                                        System.out.println("Ingrese la posicion a actualizar: ");
-                                        int pos = new Scanner(System.in).nextInt();
-                                        System.out.println("Ingrese campo que desea actualizar: ");
-                                        String campo = new Scanner(System.in).nextLine();
-                                        switch (campo){
-                                            case "nombre":
-                                                System.out.println("Nuevo nombre: ");
-                                                flores.get(pos).setNombre(new Scanner(System.in).nextLine());
-                                                break;
-                                            case "aroma":
-                                                System.out.println("Nuevo aroma: ");
-                                                flores.get(pos).setAroma(new Scanner(System.in).nextLine());
-                                                break;
-                                            case "color":
-                                                System.out.println("Nuevo color: ");
-                                                flores.get(pos).setColor(new Scanner(System.in).nextLine());
-                                                break;
-                                            case "precio":
-                                                System.out.println("Nuevo precio: ");
-                                                flores.get(pos).setPrecio(new Scanner(System.in).nextDouble());
-                                                break;
-                                        }
-                                        break;
-                                    case 2:
-                                        break;
-                                }
+                            case 3:
+
                                 break;
                             case 4:
-                                // jojolete
+
                                 break;
                             default:
+                                System.out.println("Opción fuera de rango");
                                 break;
                         }
-                        break;
-                    } while ( opcionFlores != 0 );
-                case 2: break;
-                case 3: break;
-                case 4: break;
-                default: break;
+
+                    }while (opcionflor != 0);
+
+                    break;
+                case 2:
+                    System.out.println("Menu en proceso");
+                    break;
+                case 3:
+                    System.out.println("Menu en proceso");
+                    break;
+                case 4:
+                    System.out.println("Menu en proceso");
+                    break;
+                case 5:
+                    System.out.println("Menu en proceso");
+                    break;
+                default:
+                    System.out.println("Opción fuera de rango");
+                    break;
             }
         } while (opcionPrincipal != 0);
     }
--- FuncionesBasicas.java
+++ FuncionesBasicas.java
@@ -1,47 +1,42 @@
 import java.util.Scanner;
 
 public class FuncionesBasicas {
-    public static int leerEntero(String mensaje){
+    public static int leerEntero(){
         int entero = 0;
         Boolean flag = true;
         Scanner teclado;
         do {
-            System.out.println("\t"+mensaje+": ");
             teclado = new Scanner(System.in);
             try{
                 entero = teclado.nextInt();
                 flag = false;
             }catch(Exception e){
                 System.out.println("Error: Ese no es un entero válido.");
-                System.out.println("Intentalo nuevamente el proximo año.");
+                System.out.println("Intentalo nuevamente.");
             }
         }while (flag);
         return entero;
     }
-
-    public static double leerDecimal(String mensaje){
+    public static double leerDecimal(){
         double decimal = 0;
         Boolean flag = true;
         Scanner teclado;
         do {
-            System.out.println("\t"+mensaje+": ");
             teclado = new Scanner(System.in);
             try{
                 decimal = teclado.nextDouble();
                 flag = false;
             }catch(Exception e){
                 System.out.println("Error: Ese no es un entero válido.");
-                System.out.println("Intentalo nuevamente el proximo año.");
+                System.out.println("Intentalo nuevamente.");
             }
         }while (flag);
         return decimal;
     }
-
-    public static String leertexto(String mensaje){
+    public static String leertexto(){
         String texto = "";
         Scanner teclado;
 
-        System.out.println("\t"+mensaje+": ");
         teclado = new Scanner(System.in);
 
         texto = teclado.nextLine();
